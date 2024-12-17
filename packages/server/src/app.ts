78d7022c740e74e736ba599d7b1817f28f81be55
nicholas harper <nicholasharper000@gmail.com>
2024-12-17 02:49:55 -0800

    Update app.ts

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,16 +1,15 @@
 import config from "config";
 import express from "express";
+import logger from "./utils/logger";
+import socket from "./socket";
 import {createServer} from "http";
 import {Server} from "socket.io";
 import {version} from "../package.json";
-import logger from "./utils/logger";
-import socket from "./socket";
 
+const app = express();
 const port = config.get<number>("port");
 const host = config.get<string>("host");
 const corsOrigin = config.get<string>("corsOrigin");
-
-const app = express();
 const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
